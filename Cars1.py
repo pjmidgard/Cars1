81ec66fc9b8cd356a6c88ea86c59fd62b3affa9d
Java Software <pj.midgard@yahoo.com>
2022-04-22 10:19:15 +0100

    Update Cars1.py

--- Cars1.py
+++ Cars1.py
@@ -5,4 +5,4 @@ import numpy as np
 from openvino.inference_engine import IECore
 %matplotlib inline
 from matplotlib import pyplot as plt
-print("Imported Python modules successfully.')
+print("Imported Python modules successfully.")
