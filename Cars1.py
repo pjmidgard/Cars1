5d7e6d92f75a46b1275f7269e9c6f60686ef4738
Java Software <pj.midgard@yahoo.com>
2022-04-22 10:16:58 +0100

    Create Cars1.py

---
+++ Cars1.py
@@ -0,0 +1,8 @@
+import os
+import cv2
+import time
+import numpy as np
+from openvino.inference_engine import IECore
+%matplotlib inline
+from matplotlib import pyplot as plt
+print("Imported Python modules successfully.')
